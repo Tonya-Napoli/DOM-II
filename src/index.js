dcae14ae316fd3a75daf3677dcbcfad5065abd9c
Tonya Napoli <tqwerty05@gmail.com>
2023-07-27 20:20:18 -0400

    commit 2

--- src/index.js
+++ src/index.js
@@ -2,3 +2,28 @@ import './less/index.less'
 
 // Your code goes here!
 console.log('hello world')
+
+//1. Load Event
+window.onload = function (evt) {
+    console.log(`event ${evt.type} fired! Ready to go!`)
+    const heading = document.querySelector('h1')
+    heading.textContent = 'READY TO GO!!'
+
+
+//2. The Copy Event
+window.addEventListener('copy', () => {
+    navigator.clipboard.readText()
+    .then(text => {
+        heading.textContent += text
+    })
+})
+
+//3. click
+document.body.addEventListener('click', evt => {
+    evt.target.classList.toggle('mirror')
+})
+
+
+
+
+}
